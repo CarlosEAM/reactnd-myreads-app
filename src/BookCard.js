8cb2caa254419563417838b2b6ffb2f0f3b95979
CarlosEAM <me@carlosealford.com>
2020-05-25 14:41:46 +0100

    fix: BookCard component handles books missing thumbnail or authors

--- src/BookCard.js
+++ src/BookCard.js
@@ -13,14 +13,20 @@ class BookCard extends Component {
     this.props.onClick(bookID, bookSlug);
   }
   render() {
+    const {title} = this.props.aBook;
+    const thumbnail = (this.props.aBook.hasOwnProperty("imageLinks"))
+      ? this.props.aBook.imageLinks.thumbnail 
+      : <img src="#" alt="no book cover found" />
+    const authors = (this.props.aBook.hasOwnProperty("authors"))
+      ? this.props.aBook.authors.map(author => (<p key={this.props.aBook.id + author}>{author}</p>))
+      : "No authors found";
+
     return (
       <div className="book">
-        <img src={this.props.aBook.imageLinks.thumbnail} className="book-img" width="100" height="100" alt="" />
-        <p className="book-title">{this.props.aBook.title}</p>
+        <img src={thumbnail} className="book-img" width="100" height="100" alt="" />
+        <p className="book-title">{title}</p>
         <div className="book-author">
-          {this.props.aBook.authors.map(author => (
-            <p key={this.props.aBook.title.length + author.length}>{author}</p>
-          ))}
+          {authors}
         </div>
         <ShelfControlBox
           shelf={this.props.aBook.shelf}
