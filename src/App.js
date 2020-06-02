880905162eb5be1d93d0d07ddfe6747cff79282d
CarlosEAM <me@carlosealford.com>
2020-06-02 12:15:16 +0100

    style: CSS up to nanodegree style guidance and validated

--- src/App.js
+++ src/App.js
@@ -90,6 +90,14 @@ class App extends Component {
         <footer className="app-footer">
           <p>Created by Carlos EAM 2020</p>
           <p>Udacity Project</p>
+          <p>
+            <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                <img style={{border:0,width:'88px',height:'31px'}}
+                  src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                  alt="Valid CSS!"
+                />
+                </a>
+            </p>
         </footer>
       </div>
     );
